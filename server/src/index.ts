9494b7c4a4d86f30a380176c190027f5747cb02e
Nimeavles <prlf505@gmail.com>
2022-11-13 16:27:02 +0100

    add: docker-compose for running client and server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Response, Request } from 'express';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.get('/', (req: Request, res: Response) => {
   res.send("Hello World")
