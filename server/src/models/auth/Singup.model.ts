4b6428c775f595f665f7909cd32f4c0b884d2daf
Nimeavles <prlf505@gmail.com>
2022-11-19 15:58:43 +0100

    add: model for singing up users

---
+++ server/src/models/auth/Singup.model.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+import { v4 as uuid } from 'uuid'
+
+async function SingUpUserOnDB(name: string, password: string, email: string, username: string, profileImage?: string) {
+  try {
+    const prisma = new PrismaClient();
+
+    const query = await prisma.user.create({
+      data: {
+        email,
+        password,
+        name,
+        username,
+        profile_image: profileImage,
+        id: Number(uuid())
+      }
+    });
+
+    return { code: 200, message: 'User created succesfully' };
+
+  } catch (error: any) {
+    return { code: error.code || 500, message: error.message || 'Internal Server Error' }
+  }
+}
